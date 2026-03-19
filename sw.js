e426516e01b13d167fbb7e84a9bc5a85e0a6a38b
aledegreg <aledegreg@gmail.com>
2026-03-19 14:09:00 +0100

    Upgrade service worker to version 2 with cache management
    Updated service worker to version 2 and improved caching strategy.

--- sw.js
+++ sw.js
@@ -1,10 +1,39 @@
-const cacheName = 'datecalc-v1';
-const assets = ['./', './index.html', './manifest.json', './icon.png'];
+const cacheName = 'datecalc-v2'; // <--- Cambia questo numero ogni volta che aggiorni la app!
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
 
+// Installazione: scarica i nuovi file
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log('Caching assets...');
+      return cache.addAll(assets);
+    })
+  );
+  self.skipWaiting(); // Forza l'attivazione immediata
 });
 
+// Attivazione: CANCELLA le vecchie cache (fondamentale per GitHub Pages)
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch: Prova la cache, ma se fallisce vai in rete
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
